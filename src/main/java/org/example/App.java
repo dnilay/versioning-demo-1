ab16341d9fe6de2778e5ac85abf64d72632d34c2
Nilay Das <mailnilaydas@gmail.com>
2021-01-21 00:50:24 +0530

    3rd commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App
     }
     public static void main( String[] args )
     {
-        stringConcatenator.concatenate("Hello","World");
+        System.out.println(stringConcatenator.concatenate("Hello","World"));
         System.out.println("Hello explorers.\n");
         System.out.println("This program is running version "+
                 App.class.getPackage().getImplementationVersion());
