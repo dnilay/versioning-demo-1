149fb1a9ae1ded0fc89c0f33e2203c602a85785d
Nilay Das <mailnilaydas@gmail.com>
2021-01-21 00:48:05 +0530

    2nd commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,14 +1,71 @@
 package org.example;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-/**
- * Unit test for simple App.
- */
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class AppTest 
 {
+    private StringConcatenator stringConcatenator;
+    private String string1;
+    private String string2;
+    private String result;
+
+    public AppTest(String string1, String string2, String result) {
+        this.string1 = string1;
+        this.string2 = string2;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Collection testValues() {
+        return Arrays.asList(new Object[][] {
+                { "hello ", "world", "hello world" },
+                { "Code ", "with Passion!", "Code with Passion!" },
+                { "Life is ", "good", "Life is good" },
+        });
+    }
+
+    @Before
+    public void setup() {
+        stringConcatenator = new StringConcatenator();
+    }
+
+    @Test
+    public void should_concatenate_two_strings() throws Exception {
+        assertThat(stringConcatenator.concatenate(string1, string2),
+                is(result));
+    }
+
+    @Test(expected = StringTooShortException.class)
+    public void should_throw_StringTooShortException_when_less_than_2_string() {
+        String string1 = "a";
+        String string2 = "Code";
+        stringConcatenator.concatenate(string1, string2);
+    }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void should_throw_StringTooShortException_with_mesaage() throws Exception {
+        String string1 = "a";
+        String string2 = "Code";
+        expectedException.expect(StringTooShortException.class);
+        expectedException.expectMessage("Too short string:  " + string1 + " " + string2);
+        stringConcatenator.concatenate(string1, string2);
+    }
 
     @Test
     public void shouldAnswerWithTrue()
